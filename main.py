d634a4528cbb1103cbe27609a9685850fb6e96b9
ross.erskine <ross.erskine29@outlook.com>
2022-11-15 14:49:42 +0000

    successful first run of model
    With plot

--- main.py
+++ main.py
@@ -8,24 +8,32 @@ Main is the interface for which the project CNN Autoencoder will start
 @author: Ross Erskine
 """
 
-from keras.preprocessing.image import ImageDataGenerator
+
 import matplotlib.pyplot as plt
-import os
-from PIL import Image
-import Parameters as para
 
-filename = './test_images'
-param = para.Paramaters(batch_size=32)
 
-datagen = ImageDataGenerator()
+# import modules
+import Parameters as pr
+import ImageGenerator as ig
+import CNN_Autoencoder as CAE
 
-train_gen = datagen.flow_from_directory(
-    filename,
-    target_size=(param.get_image_size()),
-    batch_size=param.get_batch_size())
+params = pr.Paramaters()
+datagen = ig.train_imageGenerator()
 
-for i in range(5):
-    img, label = train_gen.next()
-    plt.figure(figsize=(6,6))
-    plt.imshow(train_gen[0][i])
-    plt.show()
+model = CAE.CAE_model()
+model.summary()
+
+
+history = model.fit(datagen, steps_per_epoch= 32 // params.get_batch_size(),epochs=32)
+
+#plot the training and validation accuracy and loss at each epoch
+loss = history.history['loss']
+#val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+#plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
