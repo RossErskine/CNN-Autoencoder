f95cbe57619de99fb37096cbf32b55e36a53aa64
ross.erskine <ross.erskine29@outlook.com>
2022-11-22 11:54:11 +0000

    changed to maxunpooling

--- CNN_Autoencoder.py
+++ CNN_Autoencoder.py
@@ -23,57 +23,47 @@ class Encoder(nn.Module):
         super().__init__()
         
         # convolutional layers
-        self.encoder_con = nn.Sequential(
-            nn.Conv2d(3, 64, 3, stride=2, padding=1),
+        self.encoder_con = nn.Sequential(                       # Layer sizes
+            nn.Conv2d(3, 64, 3, stride=2, padding=1),           # 64@ 128x128
             nn.ReLU(True),
-            nn.Conv2d(64, 32, 3, stride=2, padding=1 ),
+            nn.MaxPool2d(2, 2),                                 # 64@ 64x64
+            nn.Conv2d(64,32, 3, stride=2, padding=1 ),          # 32@ 64x64
             nn.ReLU(True),
-            nn.Conv2d(32, 16, 3, stride=2, padding=1 ),
-            nn.ReLU(True)
+            nn.MaxPool2d(2, 2),                                 # 32@ 32x32
+            nn.Conv2d(32, 16, 3, stride=2, padding=1),          # 16@ 32x32
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2)                                  # 16@ 16x16
+            
         )
-        ## Flatten layers
-        self.flatten = nn.Flatten(start_dim=1)
         
-        ## Linear layers
-        self.encoder_lin = nn.Sequential(
-            nn.Linear(3 * 3 * 16, 128),
-            nn.ReLU(True),
-            nn.Linear(128, 4)
-        )
         
     def forward(self, x):
         x = self.encoder_con(x)
-        x = self.flatten(x)
-        x = self.encoder_lin(x)
         return x
 
 class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
               
-               
-        ## Linear layers
-        self.Decoder_lin = nn.Sequential(
-            nn.Linear(128,3*3*16),
-            nn.ReLU(True),
-            nn.Linear(4, 128)
-        )
-        # Flatten layers
-        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(16, 3, 3))
+        
         # convolutional layers
-        self.Decoder_con = nn.Sequential(
-            nn.ConvTranspose2d(16, 32, 3, stride=2, output_padding=1),
+        self.decoder_con = nn.Sequential(                                  # Layer sizes
+            nn.ConvTranspose2d(16, 16, 3, stride=2, padding=1, output_padding=1),     # 16@ 16x16
             nn.ReLU(True),
-            nn.ConvTranspose2d(32, 64, 3, stride=2, output_padding=1 ),
+            nn.MaxUnpool2d(2, 2),                                    # 16@ 32x32
+            nn.ConvTranspose2d(16, 32, 3, stride=2, padding=1, output_padding=1),      # 32@ 32x32
             nn.ReLU(True),
-            nn.ConvTranspose2d(64, 3, 3, stride=2, output_padding=1 ),
-            nn.ReLU(True)
+            nn.MaxUnpool2d(2, 2),                                    # 32@ 64x64
+            nn.ConvTranspose2d(32, 64, 3, stride=2, padding=1 , output_padding=1),     # 64@ 64x64
+            nn.ReLU(True),
+            nn.MaxUnpool2d(2, 2),                                    # 64@ 128x128
+            nn.ConvTranspose2d(64, 3, 3, stride=2, padding=0 ),      # 3@ 128x128
+                        
+            
         )
         
     def forward(self, x):
-        x = self.decoder_lin(x)
-        x = self.unflatten(x)
-        x = self.decoder_conv(x)
+        x = self.decoder_con(x)
         x = torch.sigmoid(x)
         return x
 
