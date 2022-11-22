42f8e31f1911d557969013104d210e9ce219fc7b
ross.erskine <ross.erskine29@outlook.com>
2022-11-22 18:10:57 +0000

    plot function update

--- main.py
+++ main.py
@@ -65,6 +65,31 @@ def train_model(model, dataloader, device, loss_fn, optimizer):
     
     return np.mean(train_loss)
 
+########################### plot function ########################################
+def plot_ae_outputs(encoder,decoder,n=10):
+    plt.figure(figsize=(16,4.5))
+    targets = test_dataset.targets.numpy()
+    t_idx = {i:np.where(targets==i)[0][0] for i in range(n)}
+    for i in range(n):
+      ax = plt.subplot(2,n,i+1)
+      img = test_dataset[t_idx[i]][0].unsqueeze(0).to(device)
+      encoder.eval()
+      decoder.eval()
+      with torch.no_grad():
+         rec_img  = decoder(encoder(img))
+      plt.imshow(img.cpu().squeeze().numpy(), cmap='gist_gray')
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)  
+      if i == n//2:
+        ax.set_title('Original images')
+      ax = plt.subplot(2, n, i + 1 + n)
+      plt.imshow(rec_img.cpu().squeeze().numpy(), cmap='gist_gray')  
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)  
+      if i == n//2:
+         ax.set_title('Reconstructed images')
+    plt.show()
+
 ########################### Train and evaluate ########################################
 num_epochs = 30
 diz_loss = {'train_loss':[],'val_loss':[]}
