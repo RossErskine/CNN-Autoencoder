f68ff30b1d0a5480267b5b9d49e29c41a5c7d22f
ross.erskine <ross.erskine29@outlook.com>
2022-11-23 18:56:02 +0000

    added create_training_data

---
+++ create_training_data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+create training data
+
+Original dataset has 243,437 images. 
+This script is to lower the dataset size to around 2,000
+
+Created on Wed Nov 23 18:39:44 2022
+
+@author: Ross Erskine ppxre1
+"""
+
+import os
+import shutil
+
+source= '../datasets/images'
+destination = '../datasets/galaxy_training_imgs'
+
+
+def create_training_data(source= '../datasets/images', destination = '../datasets/galaxy_training_imgs'):
+    allfiles = os.listdir(source)
+    
+    for i in range(20000):
+        src_path = os.path.join(source, allfiles[i])
+        dst_path = os.path.join(destination, allfiles[i])
+        shutil.move(src_path, dst_path)
+    
+    
+if __name__ == '__main__': 
+    
+########################## Testing ##########################################
+    import unittest
+    
+    class Test_create_training_data(unittest.TestCase):
+        """Test create traing data """
+        
+        
