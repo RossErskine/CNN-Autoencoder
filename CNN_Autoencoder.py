f625bc1d1dcd2b661d3122fe94bf663c88ef54c8
ross.erskine <ross.erskine29@outlook.com>
2022-12-05 18:00:44 +0000

    Split classes into Encoder and Decoder

--- CNN_Autoencoder.py
+++ CNN_Autoencoder.py
@@ -12,13 +12,11 @@ Source: https://github.com/patrickloeber/pytorch-examples/blob/master/Autoencode
 @author: Ross Erskine ppxre1
 """
 
-import torch
+
 from torch import nn
-SIZE = 128
 
-          
 
-class Convolutional_Autoencoder(nn.Module):
+class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
         
@@ -37,7 +35,15 @@ class Convolutional_Autoencoder(nn.Module):
             nn.Conv2d(8, 4, 3, stride=2, padding=1)   # -> N, 4, 8, 8
         )
         
-        # Encoder layers
+    def forward(self, x):
+        encoded = self.encoder(x)
+        return encoded
+    
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        # Deccoder layers
         # N, 4, 8, 8
         self.decoder = nn.Sequential(  
             nn.ConvTranspose2d(4, 8, 3, stride=2, padding=1, output_padding=1), # -> N, 8, 16, 16
@@ -52,42 +58,15 @@ class Convolutional_Autoencoder(nn.Module):
             nn.ConvTranspose2d(32, 3, 3, stride=2, padding=1, output_padding=1), # -> N, 3, 128, 128
             nn.Sigmoid()
         )
-        
-        
+
     def forward(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
+        decoded = self.decoder(x)
         return decoded
     
-    def get_encoded(self, x):
-        encoded = self.encoder(x)
-        return encoded
 
 
 
-if __name__ == '__main__': 
+
+
     
-############################# Testing #####################################
-    
-    """
-    Test class for CNN_Autoencoder
-    """
-    import unittest     
-    
-    class Test_CNN_Autoencoder(unittest.TestCase):
-        """Test CNN Autoencoder"""
         
-        def test_constructor(self):
-            """Test CNN Autoencoder constructor"""
-            CAE = Convolutional_Autoencoder()
-            print(CAE)
-            
-        def test_get_ecoder(self):
-            """Test Get encoder function """
-            model = Convolutional_Autoencoder()
-            encoder = model.get_encoder()
-            print(encoder)
-            
-            
-            
-    unittest.main()   
