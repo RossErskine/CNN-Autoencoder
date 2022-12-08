d26a8c05273e9bbfd44c35ab1a4a879b725a50a3
Ross.Erskine <ross.erskine29@outlook.com>
2022-12-08 20:16:50 +0000

    Working version

--- density_n_recon_error.py
+++ density_n_recon_error.py
@@ -9,7 +9,7 @@ Created on Tue Dec  6 13:14:42 2022
 
 """
 import numpy as np
-
+from encode_samples import encode_samples
 
 #Calculate density and reconstruction error to find their means values for
 #good and anomaly images. 
@@ -18,21 +18,26 @@ def calc_density_and_recon_error(encoder, decoder, batch_images, kde, loss_fn):
     
     density_list=[]
     recon_error_list=[]
+    
     for im in range(0, batch_images[1].size(dim=0)-1):
         
-        img  = batch_images[im]
-        #img = img[np.newaxis, :,:,:]
+        img  = batch_images[0][im,:,:,:]
         
         
         encoded_img = encoder(img) # Create a compressed version of the image using the encoder
         #encoded_img = encoded_img.flatten().cpu().numpy()# Flatten the compressed image
         
-        encoded_img = encoded_img.detach().numpy()
+        flattened_img = encoded_img.detach().flatten().numpy()
+        flattened_img = flattened_img[np.newaxis, :]
         
+        #encoded_img_vector = [np.reshape(img, (8192)) for img in encoded_img]
         
-        density = kde.score_samples(encoded_img)[0] # get a density score for the new image
-        reconstruction = decoder([[img]])
+    
+        
+        density = kde.score_samples(flattened_img)[0] # get a density score for the new image
+        reconstruction = decoder(encoded_img)
         reconstruction_error = loss_fn(reconstruction, img)
+        reconstruction_error = reconstruction_error.detach().numpy()
         density_list.append(density)
         recon_error_list.append(reconstruction_error)
         
