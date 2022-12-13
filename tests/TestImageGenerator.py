3bba1037c5a1277bef859af2e2b9edbd66ac8b24
Ross.Erskine <ross.erskine29@outlook.com>
2022-12-13 20:02:06 +0000

    added test image generator
    tested!

---
+++ tests/TestImageGenerator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Test class for ImageGenerator.py 
+Tests default file path 
+
+Created on Tue Dec 13 19:42:50 2022
+
+@author: roscopikotrain
+"""
+
+import unittest
+import os
+
+    
+class TestImageGenerator(unittest.TestCase):
+    """ Test ImageGenerator class """
+    
+    def testFilePath(self):
+        """Test default file path"""
+        filename = './test_images'
+        msg = "File path is not True"
+        self.assertTrue(os.path.exists(filename), msg)
