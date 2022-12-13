01a7940e6d3089560d2e8245e5f1d917139e57b9
Ross.Erskine <ross.erskine29@outlook.com>
2022-12-13 19:24:06 +0000

    Cleaned and commented

--- create_training_data.py
+++ create_training_data.py
@@ -4,9 +4,10 @@
 create training data
 
 Original dataset has 243,437 images. 
-This script is to lower the dataset size to around 2,000
+This script is to lower the dataset size to around 20,000
 
 Created on Wed Nov 23 18:39:44 2022
+Last commited: 13/12/22
 
 @author: Ross Erskine ppxre1
 """
@@ -14,25 +15,17 @@ Created on Wed Nov 23 18:39:44 2022
 import os
 import shutil
 
-source= '../datasets/galaxy_images'
-destination = '../datasets/galaxy_training_imgs'
-
-
-def create_training_data(source= '../datasets/galaxy_images', destination = '../datasets/galaxy_training_imgs'):
+def create_training_data(source= '../datasets/galaxy_images',
+                         destination = '../datasets/galaxy_training_imgs'):
+    """Moves a specified amount of objects from on file to another"""
+    
     allfiles = os.listdir(source)
     
-    for i in range(5000):
+    for i in range(20000):
         src_path = os.path.join(source, allfiles[i])
         dst_path = os.path.join(destination, allfiles[i])
         shutil.move(src_path, dst_path)
-    
-    
-if __name__ == '__main__': 
-    
-########################## Testing ##########################################
-    import unittest
-    
-    class Test_create_training_data(unittest.TestCase):
-        """Test create traing data """
+     
+
         
         
